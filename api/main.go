85d11a89f2c1368f5b254ce067f66fb668ae5a14
Adrian Bao <adrianbao14@gmail.com>
2020-03-17 19:36:39 -0700

    Add /exercises endpoint with valid db connection

--- api/main.go
+++ api/main.go
@@ -1,17 +1,59 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
-// GET
-func get(w http.ResponseWriter, r *http.Request) {
+// Endpoint: /exercises
+// Response: Collection of all exercises within the database
+func exercises(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "GET called"}`))
+
+	type Exercise struct {
+		ID          int            `json:"id"`
+		Name        string         `json:"name"`
+		Category    string         `json:"category"`
+		Description sql.NullString `json:"description"`
+	}
+
+	type Collection struct {
+		Collection []Exercise `json:"collection"`
+	}
+
+	db, err := sql.Open("mysql", "root:password@tcp(db:3306)/fitnessdb")
+	if err != nil {
+		fmt.Println("[ERROR]: Unable to connect to database!")
+		panic(err)
+	}
+
+	rows, err := db.Query(`SELECT * FROM exercises`)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+	collection := Collection{}
+	for rows.Next() {
+		exercise := Exercise{}
+		if err = rows.Scan(&exercise.ID, &exercise.Name, &exercise.Category, &exercise.Description); err != nil {
+			panic(err)
+		}
+		collection.Collection = append(collection.Collection, exercise)
+	}
+	json.NewEncoder(w).Encode(collection)
+
+	if err = rows.Err(); err != nil {
+		panic(err)
+	}
+	defer db.Close()
 }
 
 // POST
@@ -48,7 +90,7 @@ func main() {
 	// Using subrouter for easier migration from API v1 to v2
 	api := r.PathPrefix("/api/v1").Subrouter()
 
-	api.HandleFunc("", get).Methods(http.MethodGet)
+	api.HandleFunc("/exercises", exercises).Methods(http.MethodGet)
 	api.HandleFunc("", post).Methods(http.MethodPost)
 	api.HandleFunc("", put).Methods(http.MethodPut)
 	api.HandleFunc("", delete).Methods(http.MethodDelete)
