370944527d9c10b202609049e6c4dbf8b1a24d21
Adrian Bao <adrianbao14@gmail.com>
2020-03-23 22:42:35 -0700

    Update initial api setup

--- api/main.go
+++ api/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/BaoAdrian/fitness-api/api/app"
 	"github.com/BaoAdrian/fitness-api/api/db"
 
@@ -8,15 +11,17 @@ import (
 )
 
 func main() {
-	database, err := db.CreateDatabase()
+	// Create instance from database container (container_name: 'db')
+	database, err := db.CreateDatabase("db:3306")
 	if err != nil {
 		panic(err)
 	}
 
 	app := &app.App{
-		Router:   mux.NewRouter(),
+		Router:   mux.NewRouter().StrictSlash(true),
 		Database: database,
 	}
 
 	app.SetupRouter()
+	log.Fatal(http.ListenAndServe(":8080", app.Router))
 }
