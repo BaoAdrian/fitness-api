2158e1985eca9761e17b1b146b50ad2691ccc0f3
Adrian Bao <adrianbao14@gmail.com>
2020-04-02 13:18:52 -0700

    Created sqldump and script to populate database

--- populate-db.sh
+++ populate-db.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-docker cp ./db/create-exercises.sql db:/
-docker exec db /bin/sh -c 'mysql -uroot -ppassword </create-exercises.sql'
+# Create Database
+docker exec -i db /bin/sh -c 'mysql -uroot -ppassword -e "CREATE DATABASE IF NOT EXISTS fitnessdb"'
+
+# Populate with sql dump (if exists, otherwise, database will be empty)
+if [ -f dump.sql ]
+then
+	cat dump.sql | docker exec -i db /usr/bin/mysql -uroot -ppassword fitnessdb		
+fi
